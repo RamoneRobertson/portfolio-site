7927581ea90b0a6e66d218eda6359bb7db5ecfc8
Ramone Robertson <ramonerobertson10@gmail.com>
2024-09-21 13:54:31 +0900

    Added more styling to projects page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add('is-visible');
-      } else {
-        entry.target.classList.remove('is-visible');
       }
     });
   });
