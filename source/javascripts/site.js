e7a937127b93c622f15e5225b7e1e85302b44eea
Ramone Robertson <ramonerobertson10@gmail.com>
2024-09-29 17:45:47 +0900

    Fixed animation for proj img

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add('is-visible');
+      } else {
+        entry.target.classList.remove('is-visible');
       }
     });
   });
